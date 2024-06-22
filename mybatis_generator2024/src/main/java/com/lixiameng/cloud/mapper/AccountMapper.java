a22fe46e134daa9cdd8b05958db1166d2e26dc86
Chitandaaaaa <707320242@qq.com>
2024-06-23 01:51:22 +0800

    新增笔记文档

---
+++ mybatis_generator2024/src/main/java/com/lixiameng/cloud/mapper/AccountMapper.java
@@ -0,0 +1,7 @@
+package com.lixiameng.cloud.mapper;
+
+import com.lixiameng.cloud.entities.Account;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AccountMapper extends Mapper<Account> {
+}
